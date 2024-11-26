cabe50bfcce3692a96acbe6b32e9f94037dccfcb
lilalunex <alexanderscharow@icloud.com>
2024-11-26 13:50:33 +0100

    partner responsive + increase header font-size

--- app/routes/partner.tsx
+++ app/routes/partner.tsx
@@ -4,10 +4,10 @@ export default function Partner() {
       <h2 className="text-center py-5">Partner</h2>
 
       <div className="py-5 container mx-auto">
-        <div className="flex items-center justify-center space-x-8">
-          <img src="/partner/dukkart.png" className="rounded-3xl w-96" alt="Sergej Dukkardt" />
+        <div className="md:flex items-center justify-center md:space-x-8">
+          <img src="/partner/dukkart.png" className="rounded-3xl md:w-96" alt="Sergej Dukkardt" />
           <div className="text-center">
-            <h2 className="pt-3">Sergej Dukkardt</h2>
+            <h3 className="pt-3">Sergej Dukkardt</h3>
             <p className="px-3 mx-auto">Photographer. Media Creator. Rental Studio.</p>
             <div className="pt-3 link-holder">
               <a
@@ -43,35 +43,39 @@ export default function Partner() {
         </div>
       </div>
 
-      <div className="py-5 container mx-auto">
-        <div className="flex items-center justify-center space-x-8">
-          <div className="text-center">
-            <h2 className="pt-3">Reza Jaxon</h2>
-            <p className="px-3 mx-auto">Dancer. Graffiti Artist. Designer.</p>
-            <div className="pt-3 link-holder">
-              <a
-                href="https://shibuy.art/"
-                className="btn border-2 py-4 px-6 rounded-full"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Website
-              </a>
-            </div>
-            <div className="pt-5 link-holder">
-              <a
-                href="https://www.instagram.com/jaxon_the_6th/"
-                className="btn border-2 py-4 px-6 rounded-full"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Instagram
-              </a>
-            </div>
+      <div className="flex flex-col md:flex-row-reverse items-center justify-center md:space-x-8 w-full">
+        <img
+          src="/partner/jaxon.png"
+          className="rounded-3xl w-full md:w-96 mb-4 md:mb-0"
+          alt="Reza Jaxon"
+        />
+
+        <div className="text-center w-full md:w-auto">
+          <h3 className="pt-3">Reza Jaxon</h3>
+          <p className="px-3 mx-auto">Dancer. Graffiti Artist. Designer.</p>
+          <div className="pt-3 link-holder">
+            <a
+              href="https://shibuy.art/"
+              className="btn border-2 py-4 px-6 rounded-full"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Website
+            </a>
           </div>
-          <img src="/partner/jaxon.png" className="rounded-3xl w-96" alt="Reza Jaxon" />
+          <div className="pt-5 link-holder">
+            <a
+              href="https://www.instagram.com/jaxon_the_6th/"
+              className="btn border-2 py-4 px-6 rounded-full"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
